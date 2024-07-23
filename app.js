9ba1dc25ba0ecbf10da8e5166f4167f3a16feb6f
orpartush <orpt.dev@gmail.com>
2024-07-24 00:09:27 +0300

    add ws

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const http = require("http");
+const http = require("node:http");
 
-http.createServer(function (req, res) {
-	res.write("On the way to being a full stack engineer!");
-	res.end();
-}).listen(3000);
+http
+  // biome-ignore lint/complexity/useArrowFunction: <explanation>
+  .createServer(function (req, res) {
+    res.write("On the way to being a full stack engineer!");
+    res.end();
+  })
+  .listen(3000);
 
 console.log("Server started on port 3000!");
